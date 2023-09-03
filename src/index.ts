486a5336b6a18911097b0ba957a6c29bd54dbcd8
Anas Zain <anas.z@wethemaker.dev>
2023-09-03 09:56:05 +0300

    test commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
+import {
+  NativeModulesProxy,
+  EventEmitter,
+  Subscription,
+} from "expo-modules-core";
 
 // Import the native module. On web, it will be resolved to ReactNativeArcChartView.web.ts
 // and on native platforms to ReactNativeArcChartView.ts
-import ReactNativeArcChartViewModule from './ReactNativeArcChartViewModule';
-import ReactNativeArcChartView from './ReactNativeArcChartView';
-import { ChangeEventPayload, ReactNativeArcChartViewProps } from './ReactNativeArcChartView.types';
+import ReactNativeArcChartViewModule from "./ReactNativeArcChartViewModule";
+import ReactNativeArcChartView from "./ReactNativeArcChartView";
+import {
+  ChangeEventPayload,
+  ReactNativeArcChartViewProps,
+} from "./ReactNativeArcChartView.types";
 
 // Get the native constant value.
 export const PI = ReactNativeArcChartViewModule.PI;
@@ -17,10 +24,18 @@ export async function setValueAsync(value: string) {
   return await ReactNativeArcChartViewModule.setValueAsync(value);
 }
 
-const emitter = new EventEmitter(ReactNativeArcChartViewModule ?? NativeModulesProxy.ReactNativeArcChartView);
+const emitter = new EventEmitter(
+  ReactNativeArcChartViewModule ?? NativeModulesProxy.ReactNativeArcChartView
+);
 
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
+export function addChangeListener(
+  listener: (event: ChangeEventPayload) => void
+): Subscription {
+  return emitter.addListener<ChangeEventPayload>("onChange", listener);
 }
 
-export { ReactNativeArcChartView, ReactNativeArcChartViewProps, ChangeEventPayload };
+export {
+  ReactNativeArcChartView,
+  ReactNativeArcChartViewProps,
+  ChangeEventPayload,
+};
